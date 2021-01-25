9bcab173056f9a17b7d6dc5322d0b2a551311ff8
KucukdogruMerve <33333935+Kucukdogru@users.noreply.github.com>
2021-01-25 22:34:26 +0300

    20 Ocak Ders Sonu

---
+++ OOP1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product
+            {
+                Id = 2,
+                CategoryId =5,
+                UnitsInStock=5,
+                ProductName="Kalem",
+                UnitPrice=35
+            };
+
+            //instance
+            //PascalCase  //camelCase
+            //case sensitive
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+            //productManager.Sum2(3, 6);
+
+            //int result = productManager.Sum(3, 6);
+
+            //Console.WriteLine(result*2);
+
+        }
+    }
+}
