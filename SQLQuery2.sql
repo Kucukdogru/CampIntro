bc1d8f34eadf4515d1050144e514563459428116
KucukdogruMerve <33333935+Kucukdogru@users.noreply.github.com>
2021-01-31 16:03:03 +0300

    Add LinqProject

---
+++ SQLQuery2.sql
@@ -0,0 +1,46 @@
+﻿-- SELECT
+-- ANSII
+SELECT ContactName Ad, CompanyName SirketAdi, City Sehir FROM Customers
+
+SELECT * FROM Customers WHERE City='London'
+
+-- CASE INSENSITIVE
+SELECT * FROM Products WHERE CategoryID=1 OR CategoryID=3
+
+SELECT * FROM Products WHERE CategoryID=1 AND UnitPrice>=10
+
+SELECT * FROM Products WHERE CategoryID=1 ORDER BY UnitPrice DESC   -- ASCENDING  -- DESCENDING
+
+SELECT COUNT(*) PIECE FROM Products WHERE CategoryID=2
+
+-- Hangi kategoride kaç farklı ürünümüz var?
+SELECT CategoryID, COUNT(*) FROM Products
+GROUP BY CategoryID
+
+-- Ürün sayısı 10 dan az olan kategorileri listele
+SELECT CategoryID, COUNT(*) 
+FROM Products
+WHERE UnitPrice>20
+GROUP BY CategoryID
+HAVING COUNT(*)<10
+
+
+SELECT Products.ProductID, Products.ProductName, Products.UnitPrice, Categories.CategoryName
+FROM Products
+INNER JOIN Categories ON Products.CategoryID=Categories.CategoryID
+WHERE Products.UnitPrice>10
+-- DTO DATA TRANSFROMATION OBJECT
+
+-- Hiç satış yapamadığımız ürünler nelerdir?
+SELECT *
+FROM Products P
+LEFT JOIN [Order Details] OD ON P.ProductID=OD.ProductID
+INNER JOIN Orders O ON O.OrderID=OD.OrderID
+
+
+-- Hangi müşteriler bizden hiç ürün almamış
+SELECT * FROM Customers C
+LEFT JOIN Orders O ON C.CUSTOMERID=O.CUSTOMERID
+WHERE O.CustomerID IS NULL
+
+
